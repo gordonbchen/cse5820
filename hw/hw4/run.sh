a2b301c0cb667eb26c8da52bbd2deff84961b752
gordonbchen <gordonbchen@gmail.com>
2026-03-31 02:47:04 -0400

    log dir

--- hw/hw4/run.sh
+++ hw/hw4/run.sh
@@ -1,9 +1,9 @@
-python3 -u main.py --train_pg --episode=5000 -lr=0.003 --model_name=lr0003 | tee lr0003.txt
+python3 -u main.py --train_pg --episode=5000 -lr=0.003 --model_name=lr0003 | tee runs/lr0003/log.txt
 
-python3 -u main.py --train_pg --episode=5000 -lr=0.003 --model_name=lr0003_v1 | tee lr0003_v1.txt
+python3 -u main.py --train_pg --episode=5000 -lr=0.003 --model_name=lr0003_v1 | tee runs/lr0003_v1/log.txt
 
-python3 -u main.py --train_pg --episode=5000 -lr=0.003 --model_name=lr0003_v2 | tee lr0003_v2.txt
+python3 -u main.py --train_pg --episode=5000 -lr=0.003 --model_name=lr0003_v2 | tee runs/lr0003_v2/log.txt
 
-python3 -u main.py --train_pg --episode=5000 -lr=0.001 --model_name=lr0001 | tee lr0001.txt
+python3 -u main.py --train_pg --episode=5000 -lr=0.001 --model_name=lr0001 | tee runs/lr0001/log.txt
 
-python3 -u main.py --train_pg --episode=5000 -lr=0.001 --model_name=lr0001_v1 | tee lr0001_v1.txt
+python3 -u main.py --train_pg --episode=5000 -lr=0.001 --model_name=lr0001_v1 | tee runs/lr0001_v1/log.txt
