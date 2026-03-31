32ea6d85a99e223308acd7c68f09b9818768b7bd
gordonbchen <gordonbchen@gmail.com>
2026-03-31 18:08:46 -0400

    run lr0001

--- hw/hw4/run.sh
+++ hw/hw4/run.sh
@@ -1,3 +1,2 @@
 python3 -u main.py --train_pg --episode=5000 -lr=0.001 --model_name=lr0001 | tee runs/lr0001/log.txt
 
-python3 -u main.py --train_pg --episode=5000 -lr=0.003 --model_name=lr0003_2 | tee runs/lr0003_2/log.txt
